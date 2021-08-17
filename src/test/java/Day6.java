c9199c6ed57ac268dd27ff604058981414f5cca4
owel <rwlllaguitan@yahoo.com>
2021-08-17 18:31:51 +0800

    add comments step by step paggamit ng DataProvider

--- src/test/java/Day6.java
+++ src/test/java/Day6.java
@@ -2,8 +2,14 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 /*
-Section 20 - Parameterizing Test Cases - Data Provider
-gamit yung Data Provider annotation, ipapasa natin yung values sa test case
+*Section 20 - Parameterizing Test Cases - Data Provider
+*gamit yung Data Provider annotation, ipapasa natin yung values sa test case para 3 beses magrun yung test
+* sa @Test lagyan mo ng (dataProvider = "getData")
+* sa method make sure na may parameter ka ng username, password para icatch yung ibabato ng @DataProvider
+* sa @DataProvider gawa ka ng getData method with Object[][] return, storage lang talaga ito
+* declare ka ng ka Object 2 dimensional array
+* Object[3][2] - 1 param kung ilang beses magrrun, 2 param field na fifillupan
+* make sure may return data
 */
 public class Day6 {
 
